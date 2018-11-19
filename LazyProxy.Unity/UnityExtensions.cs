6ea808ca34abb27be10021babf75e88a0895ac10
aa <hypercodeplace@gmail.com>
2018-11-19 17:50:02 +0500

    Add non-generic RegisterLazy overload.

--- LazyProxy.Unity/UnityExtensions.cs
+++ LazyProxy.Unity/UnityExtensions.cs
@@ -71,23 +71,91 @@ namespace LazyProxy.Unity
             string name,
             Func<LifetimeManager> getLifetimeManager,
             params InjectionMember[] injectionMembers)
-            where TTo : TFrom where TFrom : class
+            where TTo : TFrom where TFrom : class =>
+            container.RegisterLazy(typeof(TFrom), typeof(TTo), name, getLifetimeManager, injectionMembers);
+
+        /// <summary>
+        /// Is used to register interface TFrom to class TTo by creation a lazy proxy at runtime.
+        /// The real class To will be instantiated only after first method execution.
+        /// </summary>
+        /// <param name="typeFrom">The binded interface.</param>
+        /// <param name="typeTo">The binded class.</param>
+        /// <param name="container">The instance of Unity container.</param>
+        /// <param name="injectionMembers">The set of injection members.</param>
+        /// <returns>The instance of Unity container.</returns>
+        public static IUnityContainer RegisterLazy(this IUnityContainer container,
+            Type typeFrom,
+            Type typeTo,
+            params InjectionMember[] injectionMembers) =>
+            container.RegisterLazy(typeFrom, typeTo, null, GetTransientLifetimeManager, injectionMembers);
+
+        /// <summary>
+        /// Is used to register interface TFrom to class TTo by creation a lazy proxy at runtime.
+        /// The real class To will be instantiated only after first method or property execution.
+        /// </summary>
+        /// <param name="typeFrom">The binded interface.</param>
+        /// <param name="typeTo">The binded class.</param>
+        /// <param name="container">The instance of Unity container.</param>
+        /// <param name="name">The registration name.</param>
+        /// <param name="injectionMembers">The set of injection members.</param>
+        /// <returns>The instance of Unity container.</returns>
+        public static IUnityContainer RegisterLazy(this IUnityContainer container,
+            Type typeFrom,
+            Type typeTo,
+            string name,
+            params InjectionMember[] injectionMembers) =>
+            container.RegisterLazy(typeFrom, typeTo, name, GetTransientLifetimeManager, injectionMembers);
+
+        /// <summary>
+        /// Is used to register interface TFrom to class TTo by creation a lazy proxy at runtime.
+        /// The real class To will be instantiated only after first method or property execution.
+        /// </summary>
+        /// <param name="typeFrom">The binded interface.</param>
+        /// <param name="typeTo">The binded class.</param>
+        /// <param name="container">The instance of Unity container.</param>
+        /// <param name="getLifetimeManager">The function instance lifetime provides.</param>
+        /// <param name="injectionMembers">The set of injection members.</param>
+        /// <returns>The instance of Unity container.</returns>
+        public static IUnityContainer RegisterLazy(this IUnityContainer container,
+            Type typeFrom,
+            Type typeTo,
+            Func<LifetimeManager> getLifetimeManager,
+            params InjectionMember[] injectionMembers) =>
+            container.RegisterLazy(typeFrom, typeTo, null, getLifetimeManager, injectionMembers);
+
+        /// <summary>
+        /// Is used to register interface TFrom to class TTo by creation a lazy proxy at runtime.
+        /// The real class To will be instantiated only after first method or property execution.
+        /// </summary>
+        /// <param name="typeFrom">The binded interface.</param>
+        /// <param name="typeTo">The binded class.</param>
+        /// <param name="container">The instance of Unity container.</param>
+        /// <param name="name">The registration name.</param>
+        /// <param name="getLifetimeManager">The function instance lifetime provides.</param>
+        /// <param name="injectionMembers">The set of injection members.</param>
+        /// <returns>The instance of Unity container.</returns>
+        public static IUnityContainer RegisterLazy(this IUnityContainer container,
+            Type typeFrom,
+            Type typeTo,
+            string name,
+            Func<LifetimeManager> getLifetimeManager,
+            params InjectionMember[] injectionMembers)
         {
             // There is no way to constraint it on the compilation step.
-            if (!typeof(TFrom).IsInterface)
+            if (!typeFrom.IsInterface)
             {
                 throw new NotSupportedException("The lazy registration is supported only for interfaces.");
             }
 
-            var lazyProxyType = LazyProxyBuilder.BuildLazyProxyType<TFrom>();
+            var lazyProxyType = LazyProxyBuilder.BuildLazyProxyType(typeFrom);
             var registrationName = Guid.NewGuid().ToString();
 
             return container
-                .RegisterType<TFrom, TTo>(registrationName, getLifetimeManager(), injectionMembers)
-                .RegisterType(typeof(TFrom), lazyProxyType, name,
+                .RegisterType(typeFrom, typeTo, registrationName, getLifetimeManager(), injectionMembers)
+                .RegisterType(typeFrom, lazyProxyType, name,
                     getLifetimeManager(),
                     new InjectionConstructor(
-                        new ResolvedParameter<Lazy<TFrom>>(registrationName))
+                        new ResolvedParameter(typeof(Lazy<>).MakeGenericType(typeFrom), registrationName))
                 );
         }
     }
